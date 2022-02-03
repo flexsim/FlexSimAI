63e1dd1d7c1fc8d3f6775ef3869c899f25a355b7
Phil BoBo <phil.bobo@flexsim.com>
2022-02-03 14:08:59 -0700

    Updated the structure of the Bonsai files.
    Split the environment into two separate files:
    (1) the flexsim_bonsai_env.py environment that communicates with FlexSim
    (2) the main.py script for starting that environment and registering it with Bonsai
    
    Added the files for installing and running within a docker container.

--- bonsai/connector/flexsim_bonsai_env.py
+++ bonsai/connector/flexsim_bonsai_env.py
@@ -6,10 +6,6 @@ import numpy as np
 
 from typing import NamedTuple, Dict, Any
 
-import time
-from microsoft_bonsai_api.simulator.client import BonsaiClient, BonsaiClientConfig
-from microsoft_bonsai_api.simulator.generated.models import SimulatorInterface, SimulatorState, SimulatorSessionResponse
-
 class FlexSimBonsaiEnv():
 
     def __init__(self, flexsimPath, modelPath, address='localhost', port=5005, verbose=False, visible=False):
@@ -196,62 +192,3 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
-
-
-def main():
-    print(f"Starting FlexSim Bonsai Environment Registration Script")
-
-    workspace = os.getenv("SIM_WORKSPACE")
-    accesskey = os.getenv("SIM_ACCESS_KEY")
-
-    config_client = BonsaiClientConfig()
-    client = BonsaiClient(config_client)
-
-    registration_info = SimulatorInterface(
-        name="FlexSim22.1",
-        timeout=60,
-        simulator_context=config_client.simulator_context,
-        description=None,
-    )
-
-    registered_session: SimulatorSessionResponse = client.session.create(workspace_name=config_client.workspace, body=registration_info)
-    print(f"Registered simulator. {registered_session.session_id}")
-
-    sequence_id = 1
-    sim_model = FlexSimBonsaiEnv(
-        flexsimPath = "C:/Program Files/FlexSim 2022 Update 1 Beta/program/flexsim.exe",
-        modelPath = "C:/Users/USERNAME/Documents/FlexSim 2022 Projects/MyModel.fsm",
-        verbose = True,
-        visible = True
-        )
-    print(f"FlexSim Environment Ready")
-    sim_model_state = { 'sim_halted': False }
-
-    try:
-        while True:
-            sim_state = SimulatorState(sequence_id=sequence_id, state=sim_model_state, halted=sim_model_state.get('sim_halted', False))
-            event = client.session.advance(
-                workspace_name=config_client.workspace,
-                session_id=registered_session.session_id,
-                body=sim_state,
-            )
-            sequence_id = event.sequence_id
-
-            if event.type == "Idle":
-                time.sleep(event.idle.callback_time)
-            elif event.type == "EpisodeStart":
-                sim_model_state = sim_model.reset(event.episode_start.config)
-            elif event.type == "EpisodeStep":
-                sim_model_state = sim_model.step(event.episode_step.action)
-            elif event.type == "EpisodeFinish":
-                sim_model_state = { 'sim_halted': False }
-            elif event.type == "Unregister":
-                print(f"Simulator Session unregistered by platform because '{event.unregister.details}'")
-                return
-    except BaseException as err:
-        client.session.delete(workspace_name=config_client.workspace, session_id=registered_session.session_id)
-        print(f"Unregistered simulator because {type(err).__name__}: {err}")
-
-
-if __name__ == "__main__":
-    main()
---
+++ bonsai/samples/ChangeoverTimes/pause.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import time
+
+def main():
+    while True:
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
